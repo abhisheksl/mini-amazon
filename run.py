8aa852111256342a1aa89e8fe616e3a3ef72f901
abhisheksl <abhisheksl.2001@gmail.com>
2018-06-21 21:55:18 +0530

    Changed file

--- run.py
+++ run.py
@@ -1,22 +1,35 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory, jsonify
 
 app = Flask('Amazon')
 
-
-@app.route('/', methods=['GET', 'POST'])
+products=[]
+@app.route('/', methods=['GET'])
 def index_page():
-    return 'Welcome to Home Page!'
+    return send_from_directory('static', 'index.html')
 
 
-@app.route('/say_hello', methods=['GET', 'POST'])
-def say_hello():
+@app.route('/api/product', methods=['GET', 'POST'])
+def product():
     if request.method == 'GET':
-        return 'Hello World' + request.args['name'] +'your age is GET '+ request.args['age']
+        query = request.args['name']
+        for prod in products:
+            if prod['name'] == query:
+                return jsonify(prod)
+        return 'No match'
 
     if request.method == 'POST':
-        return 'Hello ' + request.form['name']  +'your age is POST '+ request.form['age']
+        name = request.form['name']
+        price = request.form['price']
+        desc = request.form['desc']
+
+        prod = {
+            'name': name,
+            'price': price,
+            'desc':desc
+        }
+        products.append(prod)
+        return send_from_directory('static', 'index.html')
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='localhost', port=5000)
