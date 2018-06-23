84592176188ca0a1c7a1368c75b7c4de06e9bacb
abhisheksl <abhisheksl.2001@gmail.com>
2018-06-23 16:11:03 +0530

    used the concept of MongoDB

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, send_from_directory, jsonify
+from pymongo import MongoClient
+
 
 app = Flask('Amazon')
 
-products=[]
+client = MongoClient('localhost', port=27017)
+db = client['pymlb2-amazon']
+
 @app.route('/', methods=['GET'])
 def index_page():
     return send_from_directory('static', 'index.html')
@@ -11,13 +15,15 @@ def index_page():
 @app.route('/api/product', methods=['GET', 'POST'])
 def product():
     if request.method == 'GET':
-        query = request.args['name']
-        for prod in products:
-            if prod['name'] == query:
-                return jsonify(prod)
-        return 'No match'
+        query = {'name': request.args['name']}
+        matching_products = db['products'].find(query)
+        for p in matching_products:
+            p['_id'] = str(p['_id'])
+            return jsonify(p)
+        return 'No product found'
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
+        op_type = request.form['op_type']
         name = request.form['name']
         price = request.form['price']
         desc = request.form['desc']
@@ -25,10 +31,24 @@ def product():
         prod = {
             'name': name,
             'price': price,
-            'desc':desc
+            'desc': desc
         }
-        products.append(prod)
-        return send_from_directory('static', 'index.html')
+
+        if op_type == 'add':
+
+            db['products'].insert_one(prod)
+            return send_from_directory('static', 'index.html')
+
+        elif op_type == 'update':
+
+                filter = {'name' : request.form['name']}
+                update = {
+                    '$set': prod
+                }
+                db['products'].update_one(filter=filter, update=update, upsert=True)
+                return send_from_directory('static', 'index.html')
+
+        return 'Product not found'
 
 
 if __name__ == '__main__':
